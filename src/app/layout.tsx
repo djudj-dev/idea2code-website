97856c1000413d4a5bc45c988dd171dbe123b491
djudj-dev <vannierjulian@protonmail.com>
2025-08-28 17:58:44 +0200

    change analitycs code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics gaId="GTM-5JBQCDX6" />
+      <GoogleAnalytics gaId="G-R1MMHZNCBF" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
